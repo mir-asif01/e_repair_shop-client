ea2c1893b260f01f1058e35c190a799625b07e60
mir-asif01 <asifbsb2002@gmail.com>
2024-06-04 21:57:18 +0600

    order add feature added

--- src/components/main/Profile.jsx
+++ src/components/main/Profile.jsx
@@ -1,10 +1,24 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
 
 function Profile() {
     const { user } = useContext(AuthContext)
+    const [orders, setOrders] = useState([])
+    useEffect(() => {
+        const getAllOrders = async () => {
+            try {
+                fetch(`http://localhost:3000/users-orders?email=${user?.email}`)
+                    .then(res => res.json())
+                    .then(res => setOrders(res))
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        getAllOrders()
+    }, [])
     return <>
         <h1>Hello {user?.email}</h1>
+        <h1><span className="text-5xl">{orders ? orders.length : 0}</span> number of orders</h1>
     </>
 }
 
